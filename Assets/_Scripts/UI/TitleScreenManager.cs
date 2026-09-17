930765592fbc557f27bb37c1a3148be269e0b0e5
Ntv2201 <vubadaolop6e@gmail.com>
2026-09-17 11:04:54 +0700

    network host update

--- Assets/_Scripts/UI/TitleScreenManager.cs
+++ Assets/_Scripts/UI/TitleScreenManager.cs
@@ -1,8 +1,11 @@
+using JetBrains.Annotations;
+using Unity.Netcode;
 using UnityEngine;
 
 public class TitleScreenManager : MonoBehaviour
 {
     public void StartNetworkAsHost()
     {
+        NetworkManager.Singleton.StartHost();
     }
 }
